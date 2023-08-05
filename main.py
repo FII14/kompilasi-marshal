dde3a1214ad2b2f84cb239e9688bc373e9c3e545
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-05 09:37:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import zlib
 import base64
 
 try:
-    lokasi_file = input('Masukkan lokasi file: ')
+    lokasi_file = input("Masukkan lokasi file: ")
 except KeyboardInterrupt:
-    print('Proses kompilasi dibatalkan.')
+    print("Proses kompilasi dibatalkan.")
     exit()
 
 try:
-    nama_file_keluaran = input('Masukkan nama file keluaran: ')
+    nama_file_keluaran = input("Masukkan nama file keluaran: ")
 except KeyboardInterrupt:
-    print('Proses kompilasi dibatalkan.')
+    print("Proses kompilasi dibatalkan.")
     exit()
 
-if not nama_file_keluaran.endswith('.py'):
-    nama_file_keluaran += '.py'
+if not nama_file_keluaran.endswith(".py"):
+    nama_file_keluaran += ".py"
 
 try:
-    with open(lokasi_file, 'r') as file_sumber:
+    with open(lokasi_file, "r") as file_sumber:
         kode_sumber = file_sumber.read()
 
-    kode_terkompilasi = compile(kode_sumber, '', 'exec')
+    kode_terkompilasi = compile(kode_sumber, "", "exec")
 
     for i in range(14):
         try:
@@ -31,20 +31,20 @@ try:
             data_terenkripsi = base64.b64encode(data_terkompresi)
             kode_terkompilasi = marshal.loads(zlib.decompress(base64.b64decode(data_terenkripsi)))
         except KeyboardInterrupt:
-            print('Proses kompilasi dibatalkan.')
+            print("Proses kompilasi dibatalkan.")
             exit()
 
     if os.path.exists(nama_file_keluaran):
         try:
-            pilihan = input(f'File {nama_file_keluaran} sudah ada. Apakah Anda ingin menimpanya? (y/n): ')
-            if pilihan.lower() != 'y':
-                print('Proses kompilasi dibatalkan.')
+            pilihan = input(f"File {nama_file_keluaran} sudah ada. Apakah Anda ingin menimpanya? (y/n): ")
+            if pilihan.lower() != "y":
+                print("Proses kompilasi dibatalkan.")
                 exit()
         except KeyboardInterrupt:
-            print('Proses kompilasi dibatalkan.')
+            print("Proses kompilasi dibatalkan.")
             exit()
 
-    with open(nama_file_keluaran, 'w') as file_keluaran:
+    with open(nama_file_keluaran, "w") as file_keluaran:
         file_keluaran.write(
             f"#-------------------------------------------------\n"
             f"#!/usr/bin/env python\n"
@@ -55,15 +55,15 @@ try:
             f"exec(marshal.loads(zlib.decompress(base64.b64decode({repr(data_terenkripsi)}))))\n"
         )
 
-    print(f'File berhasil dikompilasi: {nama_file_keluaran}\n')
+    print(f"File berhasil dikompilasi: {nama_file_keluaran}\n")
 
 except FileNotFoundError:
-    print(f'File tidak ditemukan: {lokasi_file}. Pastikan Anda memasukkan lokasi file yang benar.')
+    print(f"File tidak ditemukan: {lokasi_file}. Pastikan Anda memasukkan lokasi file yang benar.")
 
 except Exception as e:
-    print(f'Terjadi kesalahan: {str(e)}')
+    print(f"Terjadi kesalahan: {str(e)}")
 
 try:
-    input('Tekan Enter untuk keluar...')
+    input("Tekan Enter untuk keluar...")
 except KeyboardInterrupt:
-    print('Proses kompilasi dibatalkan.')
+    print("Proses kompilasi dibatalkan.")
